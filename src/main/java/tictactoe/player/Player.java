05b88421c2e4017b53bd70705af51a7d90124867
Oliver Sierra <sierra.oe@gmail.com>
2025-08-14 16:30:59 -0600

    feat(player): featured implemented

--- src/main/java/tictactoe/player/Player.java
+++ src/main/java/tictactoe/player/Player.java
@@ -4,24 +4,29 @@ import tictactoe.board.Coordinate;
 import tictactoe.board.Mark;
 
 public class Player {
+    private Mark mark;
+    private String name;
+    tictactoe.utilities.ConsoleUtils console = tictactoe.utilities.ConsoleUtils.getInstance();
 
-    public Player(Mark o) {
-        // TODO Auto-generated constructor stub
+    public Player(Mark mark) {
+        this.mark = mark;
+        this.name = console.readString("Enter player " + mark + " name: ");
     }
 
     public Mark getMark() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMark'");
+        return this.mark;
     }
 
     public String getName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+        return this.name;
     }
 
     public Coordinate getMove() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getMove'");
+        console.printText("\nPlayer " + this.name + " (" + this.mark + ") turn");
+        // player try to accomplished his goals (win)
+        int row = console.readIntInRange("Enter row: ", 0, 2);
+        int col = console.readIntInRange("Enter column: ", 0, 2);
+        return new Coordinate(row, col);
     }
 
 }
